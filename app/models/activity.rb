bac54551a1e77c0f311a3b14c0387f10a4764693
Ross <ross.a@protonmail.ch>
2019-12-01 19:26:20 +0200

    Finished accocisations/validations for now

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,8 @@
 class Activity < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :image, presence: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :user_categories
+  has_many :activity_categories
+
+  validates :title, presence: true, uniqueness: true
 end
