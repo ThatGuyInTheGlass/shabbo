0656234eb7ee3382ebaf8e4bc85cb043c8e1dd3b
Ross <ross.a@protonmail.ch>
2019-12-02 14:38:34 +0200

    Finished seeds before the user&activity categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  mount_uploader :avatar, PhotoUploader
+  mount_uploader :avatar, AvatarUploader
   has_many :categories, through: :user_categories
   has_many :activities, through: :bookings
   has_many :reviews
-  validates :name, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true, uniqueness: true
   validates :last_name, presence: true, uniqueness: true
-  validates :avatar, presence: true, uniqueness: true
+  validates :avatar, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,139 @@
-user = User.create!(
-  name:"Ben Alvo",
-  email:"test@test.test",
-  password:"123456")
+puts 'Cleaning database...'
+UserCategory.destroy_all
+ActivityCategory.destroy_all
+Booking.destroy_all
+Review.destroy_all
+Activity.destroy_all
+User.destroy_all
 
-activity = Activity.create!(
-  name:"activity",
-  description: "desc",
-  price:"9999999",
-  user:user)
+############# Users ##################################
+puts 'Creating users...'
+user1 = User.create(
+  email: 'jimmy@johns.com',
+  password: '123456',
+  first_name: 'Jimbo',
+  last_name: 'Johns',
+  username: 'BigJimbo',
+  remote_avatar_url: 'https://www.iconfinder.com/icons/3389634/avatar_male_man_portrait_icon'
+  )
+user2 = User.create(
+  email: 'noah@nash.com',
+  password: '123456',
+  first_name: 'Noah',
+  last_name: 'Nash',
+  username: 'noahnash',
+  remote_avatar_url: 'https://www.iconfinder.com/icons/3389628/avatar_male_man_portrait_icon'
+  )
+user3 = User.create(
+  email: 'lindsey@henry.com',
+  password: '123456',
+  first_name: 'Lindsey',
+  last_name: 'Henry',
+  username: 'levyhenry',
+  remote_avatar_url: 'https://www.iconfinder.com/icons/3389599/afro_avatar_female_portrait_woman_icon'
+  )
+user4 = User.create(
+  email: 'ben@alvo.com',
+  password: '123456',
+  first_name: 'Ben',
+  last_name: 'Alvo',
+  username: 'ChileanPrick',
+  remote_avatar_url: 'https://www.iconfinder.com/icons/3389635/avatar_male_man_portrait_icon'
+  )
+user5 = User.create(
+  email: 'ross@allen.com',
+  password: '123456',
+  first_name: 'Ross',
+  last_name: 'Allen',
+  username: 'Ross123',
+  remote_avatar_url: 'https://www.iconfinder.com/icons/3389610/avatar_male_man_portrait_icon'
+  )
+user6 = User.create(
+  email: 'joy@phua.com',
+  password: '123456',
+  first_name: 'Joy',
+  last_name: 'Phua',
+  username: 'LeWagon1',
+  remote_avatar_url: 'https://www.iconfinder.com/icons/3389609/avatar_female_portrait_woman_icon'
+  )
+user7 = User.create(
+  email: 'david@sellam.com',
+  password: '123456',
+  first_name: 'David',
+  last_name: 'Sellam',
+  username: 'davidsellam',
+  remote_avatar_url: 'https://www.iconfinder.com/icons/3389619/avatar_beard_hipster_male_man_portrait_icon'
+  )
+############# Activities ##################################
+puts 'Creating activities...'
+activity1 = Activity.create(
+  title: 'Pita Express',
+  address: 'TLV Boardwalk',
+  description: 'One of the better pitas in Tel Aviv!',
+  remote_image_url: "https://www.google.com/url?sa=i&url=http%3A%2F%2Foishizo.blogspot.com%2F2010%2F12%2Fpita-express-more-food-court-regrets.html&psig=AOvVaw1Pt4hZmZUP18G9PDjaUsmv&ust=1575373145037000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNjesbnwluYCFQAAAAAdAAAAABAD"
+  )
 
-Booking.create!(
-  user: user,
-  activity: activity)
+activity2 = Activity.create(
+  title: 'Yoga Class',
+  address: 'Ben Yehuda 32',
+  description: 'You will feel completely enlightened',
+  remote_image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fweloveyoga.lu%2Fprices%2F&psig=AOvVaw3gOg0yTUKaCKLg3RDzgjok&ust=1575373933042000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCEmbXzluYCFQAAAAAdAAAAABAD"
+  )
+activity3 = Activity.create(
+  title: 'Surfing Lessons',
+  address: 'The Hilton Beach',
+  description: 'I will have you doing handstands on a surfboard within six weeks. Promise. Cut my dick off if im wrong!',
+  remote_image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.surf4fun.pt%2Fsurf&psig=AOvVaw0xq2KlihvG3C7tk2vp_pPX&ust=1575374009384000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMD94dXzluYCFQAAAAAdAAAAABAD"
+  )
+activity4 = Activity.create(
+  title: 'Rave!!!!',
+  address: 'The Block',
+  description: 'We know how to turn up so you can turn down baby!',
+  remote_image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alamy.com%2Fstock-photo%2Felegant-party-beautiful-people.html&psig=AOvVaw1g0uA_Zk0HoyKekPrMTmhX&ust=1575374045254000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKiL0IT0luYCFQAAAAAdAAAAABAD"
+  )
+activity5 = Activity.create(
+  title: 'Salsa Class',
+  address: 'Havana Club',
+  description: 'Taught by the famous Benji Alvo',
+  remote_image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.havanaclub.co.il%2Fenglish&psig=AOvVaw3puHyyU10I_pu8fN8EC9N4&ust=1575374389823000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDP5o31luYCFQAAAAAdAAAAABAD"
+  )
+activity6 = Activity.create(
+  title: 'Graffiti Tour',
+  address: 'The streets of florintine',
+  description: 'Completely free of charge. We only request a 20 shekel donation for logistics. Yay.',
+  remote_image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ohsoarty.com%2Ftours%2Fflorentin-graffiti-tour-public-tour-2019-gb5a8&psig=AOvVaw02qxqFNEsINjeRSmttiI73&ust=1575374336507000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKiY0vH0luYCFQAAAAAdAAAAABAD"
+  )
+############# Reviews ##################################
+puts 'Creating reviews...'
+review1 = Review.create(
+  title:"Great Job",
+  description: "This was a great service",
+  stars:"5",
+  user: user4,
+  activity: activity3
+  )
 
+review2 = Review.create(
+  title:"I dont agree with the user above",
+  description: "This was a ripoff!",
+  stars:"2",
+  user: user1,
+  activity: activity4
+  )
+############# Bookings ##################################
+puts 'Creating Bookings...'
+booking1 = Booking.create(
+  user: user1,
+  activity: activity3
+  )
+
+booking2 = Booking.create(
+  user: user2,
+  activity: activity5
+  )
+
+############# Activity Categories ##################################
+puts 'Creating Activity Categories...'
+
+################# User Categories ##################################
+puts 'Creating User Categories...'
