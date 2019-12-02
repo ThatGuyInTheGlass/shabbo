66f12ab9d02fca920a8a1c8eaf5ba64193f43b92
Ross <ross.a@protonmail.ch>
2019-12-02 11:44:45 +0200

    routes complete -removed reviews route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :activities do
-      resources :bookings
-      resources :reviews
-    end
-    resources :user_categories, except: [:index, :show]
-    resources :activity_categories, except: [:index, :show]
+  resources :activities, only: [:index, :show] do
+    resources :bookings, only: [:create, ]
+  end
+  resources :user_categories, only: [:new, :create, :edit, :update]
+  get "weekendlist", to: "dashboards#weekendlist"
+  get "profile", to: "dashboards#profile"
 end
 
 # We must make tags for user_categories & activity_categories and store them on the profile page and activity show page - SHOW ALL TAGS. Check mr.cocktail and correlate these features from the ingredients table
