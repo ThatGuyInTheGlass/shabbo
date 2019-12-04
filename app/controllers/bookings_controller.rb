6ab719b9dfd7395f22badb1cc60b8d739ccc1254
Benjamin Alvo <benjamin.alvo@gmail.com>
2019-12-04 17:42:06 +0200

    working in front end

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,19 @@
 class BookingsController < ApplicationController
   def create
+    @booking = Booking.new
+    @activity = Activity.find(params[:activity_id])
+    @booking.activity = @activity
+    @booking.user = current_user
+    if @booking.save
+      redirect_to weekendlist_path
+    end
+    authorize @booking
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.destroy
+    redirect_to weekendlist_path
   end
 end
