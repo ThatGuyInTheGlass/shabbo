c44beadaeaf958a8c1c09cb4b92823614cb3b291
Ross <ross.a@protonmail.ch>
2019-12-04 16:33:23 +0200

    changed some booking controller stuff

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,34 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def new
+    @booking = Booking.new
+    @activity = Activity.find(params[:activity_id])
+    authorize @booking
+  end
+
   def create
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.activity = Activity.find(params[:activity_id])
+    authorize @booking
+    @booking.save
+    redirect_to dashboards_path
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.destroy
+    redirect_to dashboards_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_id)
   end
 end
