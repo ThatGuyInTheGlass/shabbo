c44fbaa89c319060fbd47565b5295ba082fa0018
Benjamin Alvo <benjamin.alvo@gmail.com>
2019-12-07 19:58:47 +0200

    added the review feature. The stars for review are not working and I don't know why, tomorrow will be the first thing we will see

---
+++ app/javascript/plugins/init_star_rating.js
@@ -0,0 +1,19 @@
+// app/javascript/plugins/init_star_rating.js
+import "jquery-bar-rating";
+import "jquery-bar-rating/dist/themes/css-stars.css";
+import $ from 'jquery';
+
+
+
+const initStarRating = () => {
+  // return null, seems that this is called before the DOM is loaded
+  // console.log(document.querySelector('#review_stars'))
+  // console.log($('#review_stars'));
+  $('#review_stars').barrating({
+      theme: "css-stars"
+  });
+};
+
+export { initStarRating };
+
+
