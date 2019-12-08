009b924e12cafaa594fb593b9f719da40f5bef71
Benjamin Alvo <benjamin.alvo@gmail.com>
2019-12-08 13:03:47 +0200

    fixed the review page and the show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,7 @@ import "bootstrap";
 
 import { initStarRating } from '../plugins/init_star_rating';
 
-initStarRating();
+document.addEventListener("DOMContentLoaded", () => {
+
+  initStarRating();
+})
