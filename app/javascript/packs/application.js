31de5a4e589471612284c85b05187046324b0654
Ross <ross.a@protonmail.ch>
2019-12-08 19:02:57 +0200

    After several attempts I finally got my first instance of sweetalert - needs alot of adjustments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,14 @@
 import "bootstrap";
-
 import { initStarRating } from '../plugins/init_star_rating';
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 document.addEventListener("DOMContentLoaded", () => {
 
   initStarRating();
 })
 
+initSweetalert('#sweet-alert-demo', {
+  title: "A nice alert",
+  text: "This is a great alert, isn't it?",
+  icon: "success"
+});
